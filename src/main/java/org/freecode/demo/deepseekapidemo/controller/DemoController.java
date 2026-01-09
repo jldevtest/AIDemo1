cef201e74751eedd16248bd9b571f9c758ec7208
J Liu <jonliu6@gmail.com>
2026-01-08 23:52:21 -0800

    updated for Deepseek chat model via Open Router API

--- src/main/java/org/freecode/demo/deepseekapidemo/controller/DemoController.java
+++ src/main/java/org/freecode/demo/deepseekapidemo/controller/DemoController.java
@@ -1,7 +1,6 @@
 package org.freecode.demo.deepseekapidemo.controller;
 
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,17 +9,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    private final DeepSeekChatModel chatModel;
+    // private final DeepSeekChatModel chatModel;
 
-    @Autowired
-	public DemoController(DeepSeekChatModel chatModel) {
-		this.chatModel = chatModel;
+    // @Autowired
+	// public DemoController(DeepSeekChatModel chatModel) {
+	// 	this.chatModel = chatModel;
+	// }
+
+	// @GetMapping("/chat")
+	// public String askSomething(@RequestParam("prompt") String promptString) {
+	// 	var prompt = new Prompt(promptString);
+    //     var response = chatModel.call(prompt);
+	// 	return response.getResult().getOutput().getText();
+	// }
+
+	// private final ChatClient chatClient;
+	private final ChatClient.Builder builder;
+
+	@Autowired
+	public DemoController(ChatClient.Builder builder) {
+		this.builder = builder;
 	}
 
 	@GetMapping("/chat")
-	public String askSomething(@RequestParam("prompt") String promptString) {
-		var prompt = new Prompt(promptString);
-        var response = chatModel.call(prompt);
-		return response.getResult().getOutput().getText();
+	public String askSomething(@RequestParam(value="prompt", defaultValue = "Tell me a joke" ) String promptString) {
+		// return chatClient.prompt().user(promptString).call().content();
+		// String responseString = chatClient.prompt(promptString).call().content();
+		// if (responseString == null) {
+		// 	return "No response from model";
+		// }
+		// return responseString;
+		ChatClient chatClient = builder.build();
+		String responseString = chatClient.prompt().user(promptString).call().content();
+		return responseString;
 	}
 }
